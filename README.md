bbe8454c8beb63d521b3b85f5361c211ed849a57
Jinzhao Wu <jinz@daemon.com.au>
2019-03-15 10:57:34 +1100

    Initial commit - Added API, tested working on retrieving drive data.
    https://developers.google.com/drive/api/v3/quickstart/dotnet

--- README.md
+++ README.md
@@ -1,2 +1,15 @@
 # osgd-lfs-googledrive
 Custom Large File Sync for Google Drive
+
+#remix project
+If you are remixing this project for your own, make sure you update credentials.json with your own app.
+
+#Authorisation
+The first time you run the app, it will prompt you to authorize access:
+
+1. The app will attempt to open a new window or tab in your default browser. If this fails, copy the URL from the console and manually open it in your browser.
+
+2. If you are not already logged into your Google account, you will be prompted to log in. If you are logged into multiple Google accounts, you will be asked to select one account to use for the authorization.
+
+3. Click the Accept button.
+The app will proceed automatically, and you may close the window/tab.
---
+++ src/LargeFileSync-GD/LargeFileSync-GD/Form1.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.IO;
+using System.Windows.Forms;
+using Google.Apis.Auth.OAuth2;
+using Google.Apis.Drive.v3;
+using Google.Apis.Drive.v3.Data;
+using Google.Apis.Services;
+using Google.Apis.Util.Store;
+
+namespace LargeFileSync_GD
+{
+    public partial class Form1 : Form
+    {
+        // If modifying these scopes, delete your previously saved credentials
+        // at ~/.credentials/drive-dotnet-quickstart.json
+        static string[] Scopes = { DriveService.Scope.DriveReadonly };
+        static string ApplicationName = "Large File Sync for Google Drive - Made By Open Source Game Dev";
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            UserCredential credential;
+
+            using (var stream =
+                new FileStream("credentials.json", FileMode.Open, FileAccess.Read))
+            {
+                // The file token.json stores the user's access and refresh tokens, and is created
+                // automatically when the authorization flow completes for the first time.
+                string credPath = "token.json";
+                credential = GoogleWebAuthorizationBroker.AuthorizeAsync(
+                    GoogleClientSecrets.Load(stream).Secrets,
+                    Scopes,
+                    "user",
+                    CancellationToken.None,
+                    new FileDataStore(credPath, true)).Result;
+                Console.WriteLine("Credential file saved to: " + credPath);
+            }
+
+            // Create Drive API service.
+            var service = new DriveService(new BaseClientService.Initializer()
+            {
+                HttpClientInitializer = credential,
+                ApplicationName = ApplicationName,
+            });
+
+            // Define parameters of request.
+            FilesResource.ListRequest listRequest = service.Files.List();
+            listRequest.PageSize = 10;
+            listRequest.Fields = "nextPageToken, files(id, name)";
+
+            // List files.
+            IList<Google.Apis.Drive.v3.Data.File> files = listRequest.Execute()
+                .Files;
+            Console.WriteLine("Files:");
+            if (files != null && files.Count > 0)
+            {
+                foreach (var file in files)
+                {
+                    Console.WriteLine("{0} ({1})", file.Name, file.Id);
+                }
+            }
+            else
+            {
+                Console.WriteLine("No files found.");
+            }
+            Console.Read();
+        }
+    }
+}
